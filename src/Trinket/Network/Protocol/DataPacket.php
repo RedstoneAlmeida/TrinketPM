16a36eacd4cd616d75ff2617f0f237317ec3c4b2
ImagicalGamer <imagicalgamer@outlook.com>
2017-05-18 18:59:29 -0400

    Whoops
    Forgot to add/fix that

--- src/Trinket/Network/Protocol/DataPacket.php
+++ src/Trinket/Network/Protocol/DataPacket.php
@@ -9,24 +9,25 @@ namespace Trinket\Network\Protocol;
 
 class DataPacket{
   
-  public $id, $error, $password, $data, $reason, $chat, $selection, $serverId;
+  public $id, $error, $password, $data, $reason, $chat, $selection, $serverId, $to;
 
   public function __construct() {
     $this->id = Info::TYPE_PACKET_DUMMY;
-    $this->error = '';
-    $this->password = '';
-    $this->data = '';
-    $this->reason = '';
-    $this->chat = '';
-    $this->selection = '';
-    $this->serverId = '';
+    $this->error = Info::TYPE_ERROR_EMPTY;
+    $this->password = Info::TYPE_STRING_EMPTY;
+    $this->data = Info::TYPE_DATA_EMPTY;
+    $this->reason = Info::TYPE_STRING_EMPTY;
+    $this->chat = Info::TYPE_STRING_EMPTY;
+    $this->selection = Info::TYPE_SELECTION_PLAYERS_ALL;
+    $this->serverId = Info::TYPE_STRING_EMPTY;
+    $this->to = Info::TYPE_STRING_EMPTY;
   }
 
   public function getId() {
-    return $this->identifier;
+    return $this->id;
   }
   
   public function encode() {
-    return str_pad(json_encode(["id" => $this->id, "error" => $this->error, "password" => $this->password, "data" => $this->data, "protocol" => Info::PROTOCOL, "reason" => $this->reason, "chat" => $this->chat, "selection" => $this->selection, "serverId" => $this->serverId]), 1024);
+    return str_pad(json_encode(["id" => $this->id, "error" => $this->error, "password" => $this->password, "data" => $this->data, "protocol" => Info::PROTOCOL, "reason" => $this->reason, "chat" => $this->chat, "selection" => $this->selection, "serverId" => $this->serverId, "to" => $this->to]), 1024);
   }
 }
